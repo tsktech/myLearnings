8ed0d583e134c7015616c921e163dfe5090fdb1b
tskamath <tsktech.in@gmail.com>
2020-01-24 06:38:17 +0530

    echo print and print_r
    outputs of these functions

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+// https://www.php.net/manual/en/book.pdo.php
+?>
+<?php
 // When using prepared statements there is no official PDO feature to show you the final query string that is submitted to a database complete with the parameters you passed.
 
 // Use this simple function for debugging. The values you are passing may not be what you expect.
@@ -27,8 +30,9 @@ function build_pdo_query($string, $array) {
 echo build_pdo_query($query, $params);    //UPDATE users SET name = 'foobear' WHERE id = 1001
 ?>
 
-//Displaying errorInfo() fields for a PDO_ODBC connection to a DB2 database
+<?php //Displaying errorInfo() fields for a PDO_ODBC connection to a DB2 database
 //
+?>
 <?php
 $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
 $stmt = $dbh->prepare('bogus sql');
@@ -37,6 +41,36 @@ if (!$stmt) {
     print_r($dbh->errorInfo());
 }
 ?>
-
-
+<?php
+/*Array...
+Value = John
+Value = Jacob
+Value = Tom
+Value = Tim
+Displaying Array Values using print...
+Value = John
+Value = Jacob
+Value = Tom
+Value = Tim
+Displaying Array Values using print_r...
+Array (
+   [0] => John
+   [1] => Jacob
+   [2] => Tom
+   [3] => Tim
+)*/
 ?>
+<?php
+   $arr = array( "John", "Jacob", "Tom", "Tim");
+   echo "Array...\n";
+   foreach( $arr as $value ) {
+      echo "Value = $value \n";
+   }
+   echo "\nDisplaying Array Values using print...\n";
+   foreach( $arr as $value ) {
+      print( "Value = $value \n");
+   }
+   echo "\nDisplaying Array Values using print_r...\n";
+   print_r($arr);
+?>
+
